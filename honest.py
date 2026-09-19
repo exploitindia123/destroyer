6eb08add7e99f8597ce20a3292daaebff88db34c
exploitindia123 <www.ashutosh1000@gmail.com>
2026-09-19 23:15:33 +0530

    Change hardcoded BOT_TOKEN_HARDCODED value
    Updated hardcoded bot token for security reasons.

--- honest.py
+++ honest.py
@@ -537,7 +537,7 @@ KEYRING_FILE  = DIRS["data"] / "keyring.json"   # tiny local cache only
 # ┌──────────────────────────────────────────────────────────────┐
 # │  BOT TOKEN  add karo.   ││
 # └──────────────────────────────────────────────────────────────┘
-BOT_TOKEN_HARDCODED = "8218159061:AAEnTt7cPGYHLSwXzOQhkb1xMsWWXr00jQM"   # ← ADD BOT TOKEN
+BOT_TOKEN_HARDCODED = "8560219653:AAEp0E_7sCXulA1PlZ9y_8dyESspQQryam0"   # ← ADD BOT TOKEN
 TOKEN = (
     os.environ.get("BOT_TOKEN")
     or os.environ.get("MAIN_BOT_TOKEN")
